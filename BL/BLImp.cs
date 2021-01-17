54ada49a0bd09b99aff183d556c416de0357c2d7
Nechama <ntgalili@gmail.com>
2021-01-17 10:33:13 +0200

    ......

--- BL/BLImp.cs
+++ BL/BLImp.cs
@@ -5,13 +5,15 @@ using DLAPI;
 using BLAPI;
 using System.Threading;
 using BO;
-//using BO;
+
 
 namespace BL
 {
     class BLImp : IBL //internal
     {
         IDL dl = DLFactory.GetDL();
+
+     
 
         #region Student
         BO.Student studentDoBoAdapter(DO.Student studentDO)
