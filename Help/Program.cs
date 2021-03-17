f026e3d06415f5a07f08a9883f69715c8f4371ea
Nechama <ntgalili@gmail.com>
2021-03-17 18:45:07 +0200

    add

--- Help/Program.cs
+++ Help/Program.cs
@@ -11,77 +11,67 @@ namespace Help
     {
         static void Main(string[] args)
         {
-            List<DO.VirtualSequence> listS = DataSource.ListOfVS;
-            List<DO.VirtualSequence> listS2 = new List<DO.VirtualSequence>();
-
-
-            string str = "MEDSTSPKQEKENQEELGETRRPWEGKTAASPQYSEPESSEPLEAKQGPETGRQSRSSRPWSPQSRAKTPLGGPAGPETSSPAPVSPREPSSSPSPLAPARQDLAAPPQSDRTTSVIPEAGTPYPDPLEQSSDKRESTPHHTSQSEGNTFQQSQQPKPHLCGRRDVSYNNAKQKELRFDVFQEEDSNSDYDLQQPAPGGSEVAPSMLEITIQNAKAYLLKTSSNSGFNLYDHLSNMLTKILNERPENAVDIFENISQDVKMAHFSKKFDALQNENELLPTYEIAEKQKALFLQGHLEGVDQELEDEIAENALPNVMESAFYFEQAGVGLGTDETYRIFLALKQLTDTHPIQRCRFWGKILGLEMNYIVAEVEFREGEDEEEVEEEDVAEERDNGESEAHEDEEDELPKSFYKAPQAIPKEESRTGANKYVYFVCNEPGRPWVKLPPVIPAQIVIARKIKKFFTGRLDAPIISYPPFPGNESNYLRAQIARISAGTHVSPLGFYQFGEEEGEEEEEAEGGRNSFEENPDFEGIQVIDLVESLSNWVHHVQHILSQGRCNWFNSIQKNEEEEEEEDEEKDDSDYIEQEVGLPLLTPISEDLEIQNIPPWTTRLSSNLIPQYAIAVLQSNLWPGAYAFSNGKKFENFYIGWGHKYSPDNYTPPVPPPVYQEYPSGPEITEMDDPSVEEEQAFRAAQEAVLLAAENEESEEDEDEEDDYD";
-
-            //for (int i = 0; i < str.Length-4; i++)
-            //{
-            //    Console.WriteLine("             new Pentamer");
-            //    Console.WriteLine("              {");
-            //    Console.WriteLine("                 FirstIndex=" + i + ",");
-            //    Console.WriteLine("                 LastIndex=" + (i + 4) + ",");
-            //    Console.WriteLine("                 ProteinName =  \"RSH4A_HUMAN\" " + ",");
-            //    Console.WriteLine("                  ProteinGI = \"5TD94\"" + ",");
-            //    Console.WriteLine("                 Sequence =\"" + str.Substring(i, 5) + "\"" + ",");
-            //    Console.WriteLine("              },");
-            //}
-
-
-            foreach (DO.Pentamer item in DataSource.ListOfPentamers)
+            string str = "MDTDSQRSHLSSFTMKLMDKFHSPKIKRTPSKKGKPAEVSVKIPEKPVNKEATDRFLPEGYPLPLDLEQQAVEFMSTSAVASRSQRQKNLSWLEEKEKEVVSALRYFKTIVDKMAIDKKVLEMLPGSASKVLEAILPLVQNDPRIQHSSALSSCYSRVYQSLANLIRWSDQVMLEGVNSEDKEMVTTVKGVIKAVLDGVKELVRLTIEKQGRPSPTSPVKPSSPASKPDGPAELPLTDREVEILNKTTGMSQSTELLPDATDEEVAPPKPPLPGIRVVDNSPPPALPPKKRQSAPSPTRVAVVAPMSRATSGSSLPVGINRQDFDVDCYAQRRLSGGSHSYGGESPRLSPCSSIGKLSKSDEQLSSLDRDSGQCSRNTSCETLDHYDPDYEFLQQDLSNADQIPQQTAWNLSPLPESLGESGSPFLGPPFQLPLGGHPQPDGPLAPGQQTDTPPALPEKKRRSAASQTADGSGCRVSYERHPSQYDNISGEDLQSTAPIPSVPYAPFAAILPFQHGGSSAPVEFVGDFTAPESTGDPEKPPPLPEKKNKHMLAYMQLLEDYSEPQPSMFYQTPQNEHIYQQKNKLLMEVYGFSDSFSGVDSVQELAPPPALPPKQRQLEPPAGKDGHPRDPSAVSGVPGKDSRDGSERAPKSPDALESAQSEEEVDELSLIDHNEIMSRLTLKQEGDDGPDVRGGSGDILLVHATETDRKDLVLYCEAFLTTYRTFISPEELIKKLQYRYEKFSPFADTFKKRVSKNTFFVLVRVVDELCLVELTEEILKLLMELVFRLVCNGELSLARVLRKNILDKVDQKKLLRCATSSQPLAARGVAARPGTLHDFHSHEIAEQLTLLDAELFYKIEIPEVLLWAKEQNEEKSPNLTQFTEHFNNMSYWVRSIIMLQEKAQDRERLLLKFIKIMKHLRKLNNFNSYLAILSALDSAPIRRLEWQKQTSEGLAEYCTLIDSSSSFRAYRAALSEVEPPCIPYLGLILQDLTFVHLGNPDYIDGKVNFSKRWQQFNILDSMRCFQQAHYDMRRNDDIINFFNDFSDHLAEEALWELSLKIKPRNITRRKTDREEKT";
+            for (int i = 0; i < str.Length-4; i++)
             {
-                if (listS.Find(s => item.Sequence == s) == null)
-                {
-                    listS.Add(item.Sequence);
-                }
-            }
-            foreach (string item in listS)
-            {
-                Console.WriteLine("            new VirtualSequence");
-                Console.WriteLine("            {");
-                Console.WriteLine($"                 Sequence={item}");
-                Console.WriteLine($"                 Size={ item.Length}");
+                Console.WriteLine("             new Pentamer");
+                Console.WriteLine("              {");
+                Console.WriteLine("                 FirstIndex=" + i + ",");
+                Console.WriteLine("                 LastIndex=" + (i + 4) + ",");
+                Console.WriteLine("                 ProteinName =  \"RPGF1_HUMAN \" " + ",");
+                Console.WriteLine("                 ProteinGI = \"13905\"" + ",");
+                Console.WriteLine("                 Sequence =\"" + str.Substring(i, 5) + "\"" + ",");
                 Console.WriteLine("            },");
             }
-            List<string> listS = new List<string>();
 
-
-            for (int i = 0; i < str.Length - 4; i++)
-            {
-                bool flag = true;
-                foreach (DO.VirtualSequence item in listS)
-                {
-                    if (item.Sequence == str.Substring(i, 5))
-                        flag = false; ;
-                }
-                if (flag)
-                {
-                    listS2.Add(new DO.VirtualSequence { Sequence = str.Substring(i, 5), Size = 5 });
-                    listS.Add(new DO.VirtualSequence { Sequence = str.Substring(i, 5), Size = 5 });
-                }
-
-            }
-            foreach (DO.VirtualSequence item in listS2)
-            {
-                Console.WriteLine("            new VirtualSequence");
-                Console.WriteLine("            {");
-                Console.WriteLine($"                 Sequence=\"" + item.Sequence + "\",");
-                Console.WriteLine($"                 Size=" + 5 + ",");
-                Console.WriteLine("                     },");
-            }
-            Console.ReadKey();
         }
-        
+        //static void Main(string[] args)
+        //{
+        //    List<DO.VirtualSequence> listS = DataSource.ListOfVS;
+        //    List<DO.VirtualSequence> listS2 = new List<DO.VirtualSequence>();
+        //    //foreach (DO.Pentamer item in DataSource.ListOfPentamers)
+        //    //{
+        //    //    if (listS.Find(s => item.Sequence == s) == null)
+        //    //    {
+        //    //        listS.Add(item.Sequence);
+        //    //    }
+        //    //}
+        //    //foreach(string item in listS)
+        //    //{
+        //    //    Console.WriteLine("            new VirtualSequence");
+        //    //    Console.WriteLine("            {");
+        //    //    Console.WriteLine($"                 Sequence={item}");
+        //    //    Console.WriteLine($"                 Size={ item.Length}");
+        //    //    Console.WriteLine("            },");
+        //    //}
+        //    //List<string> listS = new List<string>();
+        //    string str = "MDTDSQRSHLSSFTMKLMDKFHSPKIKRTPSKKGKPAEVSVKIPEKPVNKEATDRFLPEGYPLPLDLEQQAVEFMSTSAVASRSQRQKNLSWLEEKEKEVVSALRYFKTIVDKMAIDKKVLEMLPGSASKVLEAILPLVQNDPRIQHSSALSSCYSRVYQSLANLIRWSDQVMLEGVNSEDKEMVTTVKGVIKAVLDGVKELVRLTIEKQGRPSPTSPVKPSSPASKPDGPAELPLTDREVEILNKTTGMSQSTELLPDATDEEVAPPKPPLPGIRVVDNSPPPALPPKKRQSAPSPTRVAVVAPMSRATSGSSLPVGINRQDFDVDCYAQRRLSGGSHSYGGESPRLSPCSSIGKLSKSDEQLSSLDRDSGQCSRNTSCETLDHYDPDYEFLQQDLSNADQIPQQTAWNLSPLPESLGESGSPFLGPPFQLPLGGHPQPDGPLAPGQQTDTPPALPEKKRRSAASQTADGSGCRVSYERHPSQYDNISGEDLQSTAPIPSVPYAPFAAILPFQHGGSSAPVEFVGDFTAPESTGDPEKPPPLPEKKNKHMLAYMQLLEDYSEPQPSMFYQTPQNEHIYQQKNKLLMEVYGFSDSFSGVDSVQELAPPPALPPKQRQLEPPAGKDGHPRDPSAVSGVPGKDSRDGSERAPKSPDALESAQSEEEVDELSLIDHNEIMSRLTLKQEGDDGPDVRGGSGDILLVHATETDRKDLVLYCEAFLTTYRTFISPEELIKKLQYRYEKFSPFADTFKKRVSKNTFFVLVRVVDELCLVELTEEILKLLMELVFRLVCNGELSLARVLRKNILDKVDQKKLLRCATSSQPLAARGVAARPGTLHDFHSHEIAEQLTLLDAELFYKIEIPEVLLWAKEQNEEKSPNLTQFTEHFNNMSYWVRSIIMLQEKAQDRERLLLKFIKIMKHLRKLNNFNSYLAILSALDSAPIRRLEWQKQTSEGLAEYCTLIDSSSSFRAYRAALSEVEPPCIPYLGLILQDLTFVHLGNPDYIDGKVNFSKRWQQFNILDSMRCFQQAHYDMRRNDDIINFFNDFSDHLAEEALWELSLKIKPRNITRRKTDREEKT";
+        //    for (int i = 0; i < str.Length - 4; i++)
+        //    {
+        //        bool flag = true;
+        //        foreach (DO.VirtualSequence item in listS)
+        //        {
+        //            if (item.Sequence == str.Substring(i, 5))
+        //                flag = false; ;
+        //        }
+        //        if (flag)
+        //        {
+        //            listS2.Add(new DO.VirtualSequence { Sequence = str.Substring(i, 5), Size = 5 });
+        //            listS.Add(new DO.VirtualSequence { Sequence = str.Substring(i, 5), Size = 5 });
+        //        }
+
+        //    }
+        //    foreach (DO.VirtualSequence item in listS2)
+        //    {
+        //        Console.WriteLine("            new VirtualSequence");
+        //        Console.WriteLine("            {");
+        //        Console.WriteLine($"                 Sequence=\"" + item.Sequence + "\",");
+        //        Console.WriteLine($"                 Size=" + 5 + ",");
+        //        Console.WriteLine("                     },");
+        //    }
+        //    Console.ReadKey();
+        //}
+
     }
 }
 
-
-//Console.WriteLine("             new Pentamer");
-//Console.WriteLine("              {");
-//Console.WriteLine("                 FirstIndex=" + i + ",");
-//Console.WriteLine("                 LastIndex=" + (i + 4) + ",");
-//Console.WriteLine("                 ProteinName =  \"RP3A_HUMAN\" " + ",");
-////Console.WriteLine("  ProteinGI = 95825" + ",");
-//Console.WriteLine("                 Sequence =\"" + str.Substring(i, 5) + "\"" + ",");
