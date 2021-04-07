aec7a193692b7b6d8f5b38ef1d77b538149ce630
Nechama <ntgalili@gmail.com>
2021-04-07 21:42:20 +0300

    xml add proteins

--- DLAPI/IDL.cs
+++ DLAPI/IDL.cs
@@ -12,34 +12,34 @@ namespace DLAPI
     // Delete - delete an instance
     public interface IDL
     {
-        //#region Protein
-        //DO.Protein GetProtein(string ProteinGI, string ProteinName);
+        #region Protein
+        DO.Protein GetProtein(string ProteinGI, string ProteinName);
         DO.Protein GetProteinBySequence(string sequence);
         DO.Protein GetProteinByName(string name);
         DO.Protein GetProteinByGI(int numOfGI);
-        //void AddProtein(DO.Protein protein);
-        //void DeleteProtein(string ProteinGI, string ProteinName);
-        //IEnumerable<DO.Protein> GetALLProtein();
-        //#endregion
+        void AddProtein(DO.Protein protein);
+        void DeleteProtein(string ProteinGI, string ProteinName);
+        IEnumerable<DO.Protein> GetALLProtein();
+        #endregion
 
-        //#region VirtualSequence
-        //DO.VirtualSequence GetVirtualSequence(string Sequence);
-        //void AddVirtualSequence(DO.VirtualSequence VirtualSequence);
-        //void DeleteVirtualSequence(string Sequence);
-        //IEnumerable<DO.VirtualSequence> GetALLVirtualSequencesBySize(int size);
+        #region VirtualSequence
+        DO.VirtualSequence GetVirtualSequence(string Sequence);
+        void AddVirtualSequence(DO.VirtualSequence VirtualSequence);
+        void DeleteVirtualSequence(string Sequence);
+        IEnumerable<DO.VirtualSequence> GetALLVirtualSequencesBySize(int size);
         IEnumerable<DO.VirtualSequence> GetAllVirtualSequences();
-        //#endregion
+        #endregion
 
 
-        //#region Pentamer
-        //IEnumerable<DO.Pentamer> GetAllPentamer();
+        #region Pentamer
+        IEnumerable<DO.Pentamer> GetAllPentamer();
         IEnumerable<DO.Pentamer> GetPentamersBySequence(string sequence);
-       // DO.Pentamer GetPentamerByProteinGI(string ProteinGI);
-        //DO.Pentamer GetPentamerByProteinName(string ProteinName);
+        IEnumerable<DO.Pentamer> GetPentamerByProteinGI(string ProteinGI);
+        DO.Pentamer GetPentamerByProteinName(string ProteinName);
         DO.Pentamer GetPentamerBySequence(string Sequence);
-        //void AddPentamer(DO.Pentamer pentamer);
-        //void DeletePentamer(string proteinGI, string sequence, int firstIndex);
+        void AddPentamer(DO.Pentamer pentamer);
+        void DeletePentamer(string proteinGI, string sequence, int firstIndex);
         IEnumerable<DO.Pentamer> Search(DO.Pentamer pntaDO);
-        //#endregion
+        #endregion
     }
 }
